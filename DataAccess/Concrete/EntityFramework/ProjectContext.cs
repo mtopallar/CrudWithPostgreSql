0f8262420601c1d045b7e6fa4fbe6d5ef4d7ba12
Murat Topallar <topallarm@outlook.com>
2022-05-05 00:51:39 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/ProjectContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ProjectContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"User ID=postgres;Password=12345;Server=localhost;Port=5432;Database=CrudWithPostgre;Integrated Security=true;Pooling = true");
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            return Ok(_productService.GetAll());
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        { 
+            return Ok(_productService.GetById(id));
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            _productService.Add(product);
+            return Ok(product);
+        }
+        [HttpPost("update")]
+        public IActionResult Update(Product product)
+        {
+            _productService.Update(product);
+            return Ok(product);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete(Product product)
+        {
+            _productService.Delete(product);
+            return Ok("Silindi");
+        }
+    }
+}
